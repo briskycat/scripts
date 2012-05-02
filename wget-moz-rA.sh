7238d2ed216926f0aeb0919e5ae3469512e3091a
Byron Gibson <fbg111@gmail.com>
2012-05-01 20:27:08 -0700

    update

--- wget-moz-rA.sh
+++ wget-moz-rA.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bash -
 
 # wget all files of specified type from url, spoof user-agent as Mozilla 5.0 (vs wget).
 # requires three cmdline arguments:
@@ -6,5 +6,20 @@
 # 2) recursion max depth level.  default is 5.
 # 2) url to download.  Can end in /, or .html/asp/whatever.
 
+FILETYPE="pdf"
+LEVEL="5"
+URL=""
 
-wget -rA.$2 --level=$2 --user-agent="Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.3) Gecko/2008092416 Firefox/3.0.3" $3
+while getopts "filetype:ft:level:l:url:u"\
+    optionName; do
+    case "$optionName" in 
+        filetype)                   FILETYPE="$OPTARG";;
+        ft)                         FILETYPE="$OPTARG";;
+        level)                      LEVEL="$OPTARG";;
+        l)                          LEVEL="$OPTARG";;
+        url)                        URL="$OPTARG";;
+        u)                          URL="$OPTARG";;
+    esac
+done
+
+wget -rA.$FILETYPE --level=$LEVEL --user-agent="Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.3) Gecko/2008092416 Firefox/3.0.3" $URL
