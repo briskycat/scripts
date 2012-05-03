8e81c97cbadc2ea64c4965a085f5111e85958be6
Byron Gibson <fbg111@gmail.com>
2012-05-02 18:03:32 -0700

    backup before global replace with #!/usr/bin/env bash -

--- find-replace-all-in-directory.sh
+++ find-replace-all-in-directory.sh
@@ -3,4 +3,9 @@
 # Recursively scan all text files in current directory, 
 # find string $1, and replace with string $2, case sensitive.
 
-find . -type f -print0 | xargs -0 sed -i "s/$1/$2/g"
+#find . -type f -print0 | xargs -0 sed -i "s/$1/$2/g"
+find . -type f -print0 | xargs -0 sed -i "s/#!\/usr\/bin\/env\ bash/#!\/usr\/bin\/env\ bash\ -/g"
+find . -type f -print0 | xargs -0 sed -i "s/#!\/usr\/bin\/env\ bash\ -\ -\ /#!\/usr\/bin\/env\ bash\ -/g"
+find . -type f -print0 | xargs -0 sed -i "s/#!\/usr\/bin\/env\ bash\ -\ -/#!\/usr\/bin\/env\ bash\ -/g"
+find . -type f -print0 | xargs -0 sed -i "s/#!\/usr\/bin\/env\ bash\ -\ /#!\/usr\/bin\/env\ bash\ -/g"
+
--- normalize-m3u.sh
+++ normalize-m3u.sh
@@ -3,6 +3,4 @@
 # This script reformats Banshee playlists for mpd.  All that must be done is to replace
 # the relative path to the music library with the full path from /.
 
-# find *.m3u, replace ../ with ~/, save to ./new
-[ -d ./new ] || mkdir new
-find . -iname *$0* -exec mv '{}' ./new
+find /home/kurtosis/Music-type f -print0 | xargs -0 sed -i "s/\.\./\~\/Music/g"
