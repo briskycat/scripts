7e1a5908508c51d599a25fa825434e983abd42a0
Byron Gibson <fbg111@gmail.com>
2012-05-23 23:00:14 -0700

    update

--- git/git-reset-hard-n-back.sh
+++ git/git-reset-hard-n-back.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash -
 
 # roll back head by n commits
-# $2 = number of commits by which to rollback HEAD
+# $1 = number of commits by which to rollback HEAD
 # --hard rolls back not just commit log but files in working directory as well
 
-git reset --hard HEAD~$2
+LEVEL=1
+
+if [ -e $1 ]; then
+    LEVEL=$1
+fi
+
+git reset --hard HEAD~$LEVEL
