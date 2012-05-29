cffdb59e58ddb9715451460a6ce32eb502324daf
Byron Gibson <fbg111@gmail.com>
2012-05-28 19:37:35 -0700

    update

--- git/git-rm-recursive.sh
+++ git/git-rm-recursive.sh
@@ -9,9 +9,9 @@ argv=$@
 echo "${argv}"
 #git filter-branch -f --index-filter 'git rm --cached --ignore-unmatch "${argv}"'
 #git filter-branch -f --index-filter 'git rm --cached --ignore-unmatch ${argv}' --tag-name-filter 'cat' HEAD --all
-#git filter-branch -f --index-filter 'git rm --cached --ignore-unmatch .gitconfig'
+git filter-branch -f --index-filter 'git rm --cached --ignore-unmatch .gitconfig.dm14050us'
 #git filter-branch -f --index-filter 'git rm --cached --ignore-unmatch install-server.sh'
-git filter-branch -f --index-filter 'git rm --cached --ignore-unmatch archive\ 3'
+#git filter-branch -f --index-filter 'git rm --cached --ignore-unmatch archive\ 3'
 
 # after above, run:
 # git add -all
