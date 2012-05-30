ca51baa121c38cd640a2507196b290404fd94d01
Byron Gibson <fbg111@gmail.com>
2012-05-30 09:32:13 -0700

    update

--- install/scala/typesafe-stack-2.0.2-install.sh
+++ install/scala/typesafe-stack-2.0.2-install.sh
@@ -10,16 +10,15 @@ PRIORITY_LEVEL=1200
 
 # install Typesafe Stack alternative for update-alternatives --config scala
 
-update-alternatives --install $BIN/scala scala $STACK_BIN/scala $PRIORITY_LEVEL \ 
---slave $BIN/classes classes $STACK_BIN/classes \ 
---slave $BIN/fsc fsc $STACK_BIN/fsc \ 
---slave $BIN/g8 g8 $STACK_BIN/g8 \ 
---slave $BIN/giter8.properties giter8.properties $STACK_BIN/giter8.properties \ 
---slave $BIN/jansi.jar jansi.jar $STACK_BIN/jansi.jar \ 
---slave $BIN/sbt sbt $STACK_BIN/sbt \ 
---slave $BIN/sbt-launch.jar sbt-launch.jar $STACK_BIN/sbt-launch.jar \ 
---slave $BIN/sbt-launch-lib.bash sbt-launch-lib.bash $STACK_BIN/sbt-launch-lib.bash \ 
---slave $BIN/scalac scalac $STACK_BIN/scalac \ 
---slave $BIN/scaladoc scaladoc $STACK_BIN/scaladoc \ 
---slave $BIN/scalap scalap $STACK_BIN/scalap \ 
---slave $MAN/man11 man.scala $STACK_MAN/man1
+update-alternatives --install $BIN/scala scala $STACK_BIN/scala $PRIORITY_LEVEL \
+ --slave $BIN/fsc fsc $STACK_BIN/fsc \
+ --slave $BIN/g8 g8 $STACK_BIN/g8 \
+ --slave $BIN/giter8.properties giter8.properties $STACK_BIN/giter8.properties \
+ --slave $BIN/jansi.jar jansi.jar $STACK_BIN/jansi.jar \
+ --slave $BIN/sbt sbt $STACK_BIN/sbt \
+ --slave $BIN/sbt-launch.jar sbt-launch.jar $STACK_BIN/sbt-launch.jar \
+ --slave $BIN/sbt-launch-lib.bash sbt-launch-lib.bash $STACK_BIN/sbt-launch-lib.bash \
+ --slave $BIN/scalac scalac $STACK_BIN/scalac \
+ --slave $BIN/scaladoc scaladoc $STACK_BIN/scaladoc \
+ --slave $BIN/scalap scalap $STACK_BIN/scalap \
+ --slave $MAN/man11 man.scala $STACK_MAN/man1
