b33757173d806c524c8601517eb44bb49d3ebd86
Byron Gibson <fbg111@gmail.com>
2012-05-29 23:39:13 -0700

    update

---
+++ install/scala/typesafe-stack-2.0.2-remove.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash -
+
+# uninstall Typesafe Stack alternative for update-alternatives --config java
+
+
+update-alternatives --remove-all scala
+update-alternatives --remove-all classes
+update-alternatives --remove-all fsc
+update-alternatives --remove-all g8
+update-alternatives --remove-all giter8.properties
+update-alternatives --remove-all jansi.jar
+update-alternatives --remove-all sbt
+update-alternatives --remove-all sbt.bat
+update-alternatives --remove-all sbt-launch.jar
+update-alternatives --remove-all sbt-launch-lib.bash
+update-alternatives --remove-all scalac
+update-alternatives --remove-all scaladoc
+update-alternatives --remove-all scalap
+update-alternatives --remove-all man.scala
