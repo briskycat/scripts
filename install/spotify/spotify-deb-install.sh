e9cc4bab74a23937e188da879eb0386cf555bcb9
Byron Gibson <fbg111@gmail.com>
2012-06-07 00:10:45 -0700

    update

--- install/spotify/spotify-deb-install.sh
+++ install/spotify/spotify-deb-install.sh
@@ -4,14 +4,16 @@
 
 # 1. Add this line to your list of repositories by
 #    editing your /etc/apt/sources.list
-deb http://repository.spotify.com stable non-free
+sudo echo '' >> /etc/apt/sources.list
+sudo echo '# spotify linux client' >> /etc/apt/sources.list
+sudo echo 'deb http://repository.spotify.com stable non-free' >> /etc/apt/sources.list
 
 # 2. If you want to verify the downloaded packages,
 #    you will need to add our public key
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 4E9CFF4E
 
 # 3. Run apt-get update
-sudo apt-get update
+#sudo apt-get update
 
 # 4. Install spotify!
-sudo apt-get install spotify-client
+#sudo apt-get install spotify-client
