5356b7dc949b86c7b7e6a453172285499de9c6e9
Byron Gibson <fbg111@gmail.com>
2012-07-04 15:32:50 -0700

    update

--- iconv2utf8.sh
+++ iconv2utf8.sh
@@ -3,4 +3,7 @@
 # $1 = source file
 # $2 = dest file
 
+# usage:
+# sh iconv2utf8 2012-07-04-the-pathology-of-big-data.markdown 2012-07-04-the-pathology-of-big-data.markdown.utf8
+
 iconv $1 -t UTF-8 -c -o $2
