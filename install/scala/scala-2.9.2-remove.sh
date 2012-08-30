d376ddb76463f3848fba1cd20d71a4012b803dc7
Byron Gibson <fbg111@gmail.com>
2012-08-30 04:21:37 +0000

    update

--- install/scala/scala-2.9.2-remove.sh
+++ install/scala/scala-2.9.2-remove.sh
@@ -5,15 +5,7 @@
 
 update-alternatives --remove-all scala
 
-#update-alternatives --remove-all classes
 #update-alternatives --remove-all fsc
-#update-alternatives --remove-all g8
-#update-alternatives --remove-all giter8.properties
-#update-alternatives --remove-all jansi.jar
-#update-alternatives --remove-all sbt
-#update-alternatives --remove-all sbt.bat
-#update-alternatives --remove-all sbt-launch.jar
-#update-alternatives --remove-all sbt-launch-lib.bash
 #update-alternatives --remove-all scalac
 #update-alternatives --remove-all scaladoc
 #update-alternatives --remove-all scalap
