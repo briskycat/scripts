65de0eaaf0af8084c0ef70c828cfadcaf93557e9
Byron Gibson <fbg111@gmail.com>
2012-09-11 03:31:55 +0000

    update

--- install/java/oracle-jdk6-server-allslave.sh
+++ install/java/oracle-jdk6-server-allslave.sh
@@ -1,67 +1,65 @@
 #!/usr/bin/env bash -
 # tested on Ubuntu 12.04 only
-
-BIN="/usr/bin"
-MAN="/usr/share/man"
-JVM="/opt/java/jdk"
-JDK="1.6.0_35"
-JDK_BIN="$JDK/bin"
-JDK_DB_BIN="$JDK/db/bin"
-JDK_MAN="$JDK/man"
-PRIORITY_LEVEL=1200
+JVM_TAR="/usr/bin"
+JVM_SRC="/opt/java/jdk"
+JDK_VER="$JVM_SRC/1.6.0_35
+JDK_SRC="$JDK_VER/bin"
+JDK_DB_SRC="$JDK_VER/db/bin"
+MAN_SRC="$JDK_VER/man/man1"
+MAN_TAR="/usr/share/man/man10"
 
 # install Oracle JDK6 alternative for update-alternatives --config java
 
-update-alternatives --install $BIN/java java $JVM/$JDK_BIN/java $PRIORITY_LEVEL \
---slave $BIN/appletviewer appletviewer $JVM/$JDK_BIN/appletviewer \
---slave $BIN/apt apt $JVM/$JDK_BIN/apt \
---slave $BIN/ControlPanel ControlPanel $JVM/$JDK_BIN/ControlPanel \
---slave $BIN/extcheck extcheck $JVM/$JDK_BIN/extcheck \
---slave $BIN/HtmlConverter HtmlConverter $JVM/$JDK_BIN/HtmlConverter \
---slave $BIN/idlj idlj $JVM/$JDK_BIN/idlj \
---slave $BIN/jar jar $JVM/$JDK_BIN/jar \
---slave $BIN/jarsigner jarsigner $JVM/$JDK_BIN/jarsigner \
---slave $BIN/javac javac $JVM/$JDK_BIN/javac \
---slave $BIN/javadoc javadoc $JVM/$JDK_BIN/javadoc \
---slave $BIN/javah javah $JVM/$JDK_BIN/javah \
---slave $BIN/javap javap $JVM/$JDK_BIN/javap \
---slave $BIN/javaws javaws $JVM/$JDK_BIN/javaws \
---slave $BIN/jconsole jconsole $JVM/$JDK_BIN/jconsole \
---slave $BIN/jcontrol jcontrol $JVM/$JDK_BIN/jcontrol \
---slave $BIN/jdb jdb $JVM/$JDK_BIN/jdb \
---slave $BIN/jhat jhat $JVM/$JDK_BIN/jhat \
---slave $BIN/jinfo jinfo $JVM/$JDK_BIN/jinfo \
---slave $BIN/jmap jmap $JVM/$JDK_BIN/jmap \
---slave $BIN/jps jps $JVM/$JDK_BIN/jps \
---slave $BIN/jrunscript jrunscript $JVM/$JDK_BIN/jrunscript \
---slave $BIN/jsadebugd jsadebugd $JVM/$JDK_BIN/jsadebugd \
---slave $BIN/jstack jstack $JVM/$JDK_BIN/jstack \
---slave $BIN/jstat jstat $JVM/$JDK_BIN/jstat \
---slave $BIN/jstatd jstatd $JVM/$JDK_BIN/jstatd \
---slave $BIN/jvisualvm jvisualvm $JVM/$JDK_BIN/jvisualvm \
---slave $BIN/keytool keytool $JVM/$JDK_BIN/keytool \
---slave $BIN/native2ascii native2ascii $JVM/$JDK_BIN/native2ascii \
---slave $BIN/orbd orbd $JVM/$JDK_BIN/orbd \
---slave $BIN/pack200 pack200 $JVM/$JDK_BIN/pack200 \
---slave $BIN/policytool policytool $JVM/$JDK_BIN/policytool \
---slave $BIN/rmic rmic $JVM/$JDK_BIN/rmic \
---slave $BIN/rmid rmid $JVM/$JDK_BIN/rmid \
---slave $BIN/rmiregistry rmiregistry $JVM/$JDK_BIN/rmiregistry \
---slave $BIN/schemagen schemagen $JVM/$JDK_BIN/schemagen \
---slave $BIN/serialver serialver $JVM/$JDK_BIN/serialver \
---slave $BIN/servertool servertool $JVM/$JDK_BIN/servertool \
---slave $BIN/tnameserv tnameserv $JVM/$JDK_BIN/tnameserv \
---slave $BIN/unpack200 unpack200 $JVM/$JDK_BIN/unpack200 \
---slave $BIN/wsgen wsgen $JVM/$JDK_BIN/wsgen \
---slave $BIN/wsimport wsimport $JVM/$JDK_BIN/wsimport \
---slave $BIN/xjc xjc $JVM/$JDK_BIN/xjc \
---slave $BIN/dblook dblook $JVM/$JDK_DB_BIN/dblook \
---slave $BIN/ij ij $JVM/$JDK_DB_BIN/ij \
---slave $BIN/NetworkServerControl NetworkServerControl $JVM/$JDK_DB_BIN/NetworkServerControl \
---slave $BIN/setEmbeddedCP setEmbeddedCP $JVM/$JDK_DB_BIN/setEmbeddedCP \
---slave $BIN/setNetworkClientCP setNetworkClientCP $JVM/$JDK_DB_BIN/setNetworkClientCP \
---slave $BIN/setNetworkServerCP setNetworkServerCP $JVM/$JDK_DB_BIN/setNetworkServerCP \
---slave $BIN/startNetworkServer startNetworkServer $JVM/$JDK_DB_BIN/startNetworkServer \
---slave $BIN/stopNetworkServer stopNetworkServer $JVM/$JDK_DB_BIN/stopNetworkServer \
---slave $BIN/sysinfo sysinfo $JVM/$JDK_DB_BIN/sysinfo \
---slave $MAN/man10 man.java $JVM/$JDK_MAN/man1
+update-alternatives --install $JVM_TAR/java java $JDK_SRC/java $PRIORITY_LEVEL \
+--slave $JVM_TAR/appletviewer appletviewer $JDK_SRC/appletviewer \
+--slave $JVM_TAR/apt apt $JDK_SRC/apt \
+--slave $JVM_TAR/ControlPanel ControlPanel $JDK_SRC/ControlPanel \
+--slave $JVM_TAR/extcheck extcheck $JDK_SRC/extcheck \
+--slave $JVM_TAR/HtmlConverter HtmlConverter $JDK_SRC/HtmlConverter \
+--slave $JVM_TAR/idlj idlj $JDK_SRC/idlj \
+--slave $JVM_TAR/jar jar $JDK_SRC/jar \
+--slave $JVM_TAR/jarsigner jarsigner $JDK_SRC/jarsigner \
+--slave $JVM_TAR/javac javac $JDK_SRC/javac \
+--slave $JVM_TAR/javadoc javadoc $JDK_SRC/javadoc \
+--slave $JVM_TAR/javah javah $JDK_SRC/javah \
+--slave $JVM_TAR/javap javap $JDK_SRC/javap \
+--slave $JVM_TAR/javaws javaws $JDK_SRC/javaws \
+--slave $JVM_TAR/jconsole jconsole $JDK_SRC/jconsole \
+--slave $JVM_TAR/jcontrol jcontrol $JDK_SRC/jcontrol \
+--slave $JVM_TAR/jdb jdb $JDK_SRC/jdb \
+--slave $JVM_TAR/jhat jhat $JDK_SRC/jhat \
+--slave $JVM_TAR/jinfo jinfo $JDK_SRC/jinfo \
+--slave $JVM_TAR/jmap jmap $JDK_SRC/jmap \
+--slave $JVM_TAR/jps jps $JDK_SRC/jps \
+--slave $JVM_TAR/jrunscript jrunscript $JDK_SRC/jrunscript \
+--slave $JVM_TAR/jsadebugd jsadebugd $JDK_SRC/jsadebugd \
+--slave $JVM_TAR/jstack jstack $JDK_SRC/jstack \
+--slave $JVM_TAR/jstat jstat $JDK_SRC/jstat \
+--slave $JVM_TAR/jstatd jstatd $JDK_SRC/jstatd \
+--slave $JVM_TAR/jvisualvm jvisualvm $JDK_SRC/jvisualvm \
+--slave $JVM_TAR/keytool keytool $JDK_SRC/keytool \
+--slave $JVM_TAR/native2ascii native2ascii $JDK_SRC/native2ascii \
+--slave $JVM_TAR/orbd orbd $JDK_SRC/orbd \
+--slave $JVM_TAR/pack200 pack200 $JDK_SRC/pack200 \
+--slave $JVM_TAR/policytool policytool $JDK_SRC/policytool \
+--slave $JVM_TAR/rmic rmic $JDK_SRC/rmic \
+--slave $JVM_TAR/rmid rmid $JDK_SRC/rmid \
+--slave $JVM_TAR/rmiregistry rmiregistry $JDK_SRC/rmiregistry \
+--slave $JVM_TAR/schemagen schemagen $JDK_SRC/schemagen \
+--slave $JVM_TAR/serialver serialver $JDK_SRC/serialver \
+--slave $JVM_TAR/servertool servertool $JDK_SRC/servertool \
+--slave $JVM_TAR/tnameserv tnameserv $JDK_SRC/tnameserv \
+--slave $JVM_TAR/unpack200 unpack200 $JDK_SRC/unpack200 \
+--slave $JVM_TAR/wsgen wsgen $JDK_SRC/wsgen \
+--slave $JVM_TAR/wsimport wsimport $JDK_SRC/wsimport \
+--slave $JVM_TAR/xjc xjc $JDK_SRC/xjc \
+--slave $JVM_TAR/dblook dblook $JDK_DB_SRC/dblook \
+--slave $JVM_TAR/ij ij $JDK_DB_SRC/ij \
+--slave $JVM_TAR/NetworkServerControl NetworkServerControl $JDK_DB_SRC/NetworkServerControl \
+--slave $JVM_TAR/setEmbeddedCP setEmbeddedCP $JDK_DB_SRC/setEmbeddedCP \
+--slave $JVM_TAR/setNetworkClientCP setNetworkClientCP $JDK_DB_SRC/setNetworkClientCP \
+--slave $JVM_TAR/setNetworkServerCP setNetworkServerCP $JDK_DB_SRC/setNetworkServerCP \
+--slave $JVM_TAR/startNetworkServer startNetworkServer $JDK_DB_SRC/startNetworkServer \
+--slave $JVM_TAR/stopNetworkServer stopNetworkServer $JDK_DB_SRC/stopNetworkServer \
+--slave $JVM_TAR/sysinfo sysinfo $JDK_DB_SRC/sysinfo \
+--slave $MAN_TAR man.java $MAN_SRC
