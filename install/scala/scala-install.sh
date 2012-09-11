53372fe0e5074e2aaac43f77209992389dfa9578
Byron Gibson <fbg111@gmail.com>
2012-09-11 05:26:50 +0000

    update

--- install/scala/scala-install.sh
+++ install/scala/scala-install.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash -
 # tested on Ubuntu 12.04 only
 
-BIN="/usr/bin"
-MAN="/usr/share/man"
-SCALA="/opt/scala/bin/2.9.2"
-SCALA_BIN="$SCALA/bin"
-SCALA_MAN="$SCALA/man"
+SCALA_TAR="/usr/bin"
+SCALA_SRC_LOC="/opt/scala/bin"
+SCALA_SRC_VER="2.9.2"
+SCALA_SRC="$SCALA_SRC_LOC/$SCALA_SRC_VER/bin"
+MAN_SRC="$SCALA_SRC_LOC/$SCALA_SRC_VER/man/man1"
+MAN_TAR="/usr/share/man/man11"
 PRIORITY_LEVEL=1100
 
 # install Typesafe Stack alternative for update-alternatives --config scala
-
-update-alternatives --install $BIN/scala scala $SCALA_BIN/scala $PRIORITY_LEVEL \
- --slave $BIN/fsc fsc $SCALA_BIN/fsc \
- --slave $BIN/scalac scalac $SCALA_BIN/scalac \
- --slave $BIN/scaladoc scaladoc $SCALA_BIN/scaladoc \
- --slave $BIN/scalap scalap $SCALA_BIN/scalap \
- --slave $MAN/man11 man.scala $SCALA_MAN/man1
+update-alternatives --install $SCALA_TAR/scala scala $SCALA_SRC/scala $PRIORITY_LEVEL \
+ --slave $SCALA_TAR/fsc fsc $SCALA_SRC/fsc \
+ --slave $SCALA_TAR/scalac scalac $SCALA_SRC/scalac \
+ --slave $SCALA_TAR/scaladoc scaladoc $SCALA_SRC/scaladoc \
+ --slave $SCALA_TAR/scalap scalap $SCALA_SRC/scalap \
+ --slave $MAN_TAR man.scala $MAN_SRC
 
--- install/scala/typesafe-stack-install.sh
+++ install/scala/typesafe-stack-install.sh
@@ -1,24 +1,25 @@
 #!/usr/bin/env bash -
 # tested on Ubuntu 12.04 only
 
-BIN="/usr/bin"
-MAN="/usr/share/man"
-STACK="/opt/scala/typesafe/stack/2.0.2"
-STACK_BIN="$STACK/bin"
-STACK_MAN="$STACK/man"
+STACK_TAR="/usr/bin"
+STACK_SRC_LOC="/opt/scala/typesafe/stack"
+STACK_SRC_VER="2.0.2"
+STACK_SRC="$STACK_SRC_LOC/$STACK_SRC_VER/bin"
+MAN_SRC="$STACK_SRC_LOC/$STACK_SRC_VER/man"
+MAN_TAR="/usr/share/man/man11"
 PRIORITY_LEVEL=1200
 
 # install Typesafe Stack alternative for update-alternatives --config scala
 
-update-alternatives --install $BIN/scala scala $STACK_BIN/scala $PRIORITY_LEVEL \
- --slave $BIN/fsc fsc $STACK_BIN/fsc \
- --slave $BIN/g8 g8 $STACK_BIN/g8 \
- --slave $BIN/giter8.properties giter8.properties $STACK_BIN/giter8.properties \
- --slave $BIN/jansi.jar jansi.jar $STACK_BIN/jansi.jar \
- --slave $BIN/sbt sbt $STACK_BIN/sbt \
- --slave $BIN/sbt-launch.jar sbt-launch.jar $STACK_BIN/sbt-launch.jar \
- --slave $BIN/sbt-launch-lib.bash sbt-launch-lib.bash $STACK_BIN/sbt-launch-lib.bash \
- --slave $BIN/scalac scalac $STACK_BIN/scalac \
- --slave $BIN/scaladoc scaladoc $STACK_BIN/scaladoc \
- --slave $BIN/scalap scalap $STACK_BIN/scalap \
- --slave $MAN/man11 man.scala $STACK_MAN/man1
+update-alternatives --install $STACK_TAR/scala scala $STACK_SRC/scala $PRIORITY_LEVEL \
+ --slave $STACK_TAR/fsc fsc $STACK_SRC/fsc \
+ --slave $STACK_TAR/g8 g8 $STACK_SRC/g8 \
+ --slave $STACK_TAR/giter8.properties giter8.properties $STACK_SRC/giter8.properties \
+ --slave $STACK_TAR/jansi.jar jansi.jar $STACK_SRC/jansi.jar \
+ --slave $STACK_TAR/sbt sbt $STACK_SRC/sbt \
+ --slave $STACK_TAR/sbt-launch.jar sbt-launch.jar $STACK_SRC/sbt-launch.jar \
+ --slave $STACK_TAR/sbt-launch-lib.bash sbt-launch-lib.bash $STACK_SRC/sbt-launch-lib.bash \
+ --slave $STACK_TAR/scalac scalac $STACK_SRC/scalac \
+ --slave $STACK_TAR/scaladoc scaladoc $STACK_SRC/scaladoc \
+ --slave $STACK_TAR/scalap scalap $STACK_SRC/scalap \
+ --slave $MAN_TAR man.scala $MAN_SRC
