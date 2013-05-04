1a4524aeefeae538c6dce2e29a20d3c51c1aecc6
Byron Gibson <fbg111@gmail.com>
2013-05-03 23:52:13 -0700

    update

---
+++ list-processes-on-ports-lsof.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# http://stackoverflow.com/questions/14168351/cant-listen-on-port-80-with-my-server
+# show all process bound to ports
+# use sudo to show all processes
+
+lsof -n -P -i +c 15
+#lsof -i :80
---
+++ list-processes-on-ports-netstat.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# http://askubuntu.com/questions/135602/i-have-permanent-connections-to-canonical-servers-what-are-they-for-and-how-can
+# http://stackoverflow.com/questions/14168351/cant-listen-on-port-80-with-my-server
+# use sudo to show all processes
+
+netstat -lnp
+#netstat -tp
