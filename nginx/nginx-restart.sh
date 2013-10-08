0c8775544307cf1cff6803a6e228fe3c19278b19
Byron Gibson <fbg111@gmail.com>
2013-10-08 02:33:58 +0000

    updated nginx scripts with sudo service nginx X

--- nginx/nginx-restart.sh
+++ nginx/nginx-restart.sh
@@ -2,4 +2,5 @@
 
 # restart is an /etc/init.d system command that can start and/or restart nginx
 #sudo -u www-data /etc/init.d/nginx restart
-sudo /etc/init.d/nginx restart
+#sudo /etc/init.d/nginx restart
+sudo service nginx restart
--- nginx/nginx-stop.sh
+++ nginx/nginx-stop.sh
@@ -2,4 +2,6 @@
 
 # "stop" sends -TERM and nginx exits immediately.
 #sudo -U www-data /etc/init.d/nginx -s stop
-sudo /etc/init.d/nginx -s stop
+#sudo /etc/init.d/nginx -s stop
+sudo service nginx stop
+
