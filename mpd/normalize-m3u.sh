c6842095b1c20244733b73ceacc0c4821accaccb
Byron Gibson <fbg111@gmail.com>
2013-10-18 00:17:57 -0700

    update

---
+++ mpd/normalize-m3u.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# This script reformats Banshee playlists for mpd.  All that must be done is to replace
+# the relative path to the music library with the full path from /.
+
+# Usage:   sh normalize-m3u.sh playlistname.m3u
+
+EXPECTED_ARGS=1
+E_BADARGS=65
+
+if [ $EXPECTED_ARGS -ne $# ]; then
+    echo "Usage: `basename $0` {arg}"
+    exit $E_BADARGS
+else
+    #find /home/kurtosis/Music-type f -print0 | xargs -0 sed -i "s/\.\./\~\/Music/g"
+    #find . -type f -print0 | xargs -0 sed -i 's/\.\.\/\.\.\/Library/\/home\/kurtosis\/Music\/Library/g'
+    #echo "find . -type f -print0 | xargs -0 sed -i 's/\.\.\/\.\.\/Library/\/home\/kurtosis\/Music\/Library/g'"
+    echo "executing...  sed -i.bak 's/..\/Library/\/home\/kurtosis\/Music\/Library/g' $1"
+    sed -i.bak 's/..\/Library/\/home\/kurtosis\/Music\/Library/g' $1
+    
+fi
