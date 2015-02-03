266cd1ebfa70601e45704ef1d0305a38395748ad
Byron Gibson <byron@vaurum.com>
2015-02-02 20:27:20 -0800

    updated ubuntu-alternatives node.js install with keybase

--- install/node.js/node-install.sh
+++ install/node.js/node-install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash -
 
-# install haskell platform to the system user via ubuntu update-alternatives,
+# install node.js to the system user via ubuntu update-alternatives,
 # rather than as current user via .profile.  system user in this case = /usr/bin
 # tested on Ubuntu 12.04 only
 
@@ -13,4 +13,7 @@ PRIORITY_LEVEL=1200
 
 update-alternatives --install $BIN/node nodejs $NODE_BIN/node $PRIORITY_LEVEL \
  --slave $BIN/npm npm $NODE_BIN/npm \
+ --slave $BIN/nperf nperf $NODE_BIN/nperf \
+ --slave $BIN/idonethis idonethis $NODE_BIN/idonethis \
+ --slave $BIN/keybase keybase $NODE_BIN/keybase \
  --slave $MAN/man10 man.nodejs $NODE_MAN/man1
