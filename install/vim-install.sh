98a53b5ad48374cab12b1510c1e16be153fdb33a
Byron Gibson <fbg111@gmail.com>
2015-07-28 13:20:28 -0700

    update

--- install/vim-install.sh
+++ install/vim-install.sh
@@ -6,8 +6,8 @@
 
 BIN="/usr/bin"
 SHARE="/usr/share"
-MAN="/usr/man/"
-DOC="$SHARE/vim/vim74/doc"
+MAN="$SHARE/man"
+SHARE_VIM="$SHARE/vim/vim74"
 VIM="/opt/vim/7.4"
 VIM_BIN="$VIM/bin"
 VIM_SHARE="$VIM/share"
@@ -16,12 +16,12 @@ VIM_SHARE_VIM="$VIM_SHARE/vim/vim74"
 PRIORITY_LEVEL=1200
 
 update-alternatives --install $BIN/vim vim $VIM_BIN/vim $PRIORITY_LEVEL \
- --slave $BIN/ex ex $VIM_BIN/ex \
- --slave $BIN/rview rview $VIM_BIN/rview \
- --slave $BIN/rvim rvim $VIM_BIN/rvim \
- --slave $BIN/view view $VIM_BIN/view \
- --slave $BIN/vimtutor vimtutor $VIM_BIN/vimtutor \
- --slave $BIN/xxd xxd $VIM_BIN/xxd \
- --slave $DOC/doc/vim2html.pl vim2html.pl $VIM_DOC/vim2html.pl \
- --slave $DOC/macros/less.sh less.sh $VIM_SHARE_VIM/macros/less.sh \
- --slave $MAN/man10 man.vim $VIM_MAN/man1
+--slave $BIN/rvim rvim $VIM_BIN/rvim \
+--slave $BIN/vimdiff vimdiff $VIM_BIN/vimdiff \
+--slave $BIN/vimtutor vimtutor $VIM_BIN/vimtutor \
+--slave $MAN/man10 man.vim $VIM_MAN/man1
+
+update-alternatives --install $BIN/ex ex $VIM_BIN/ex $PRIORITY_LEVEL
+update-alternatives --install $BIN/rview rview $VIM_BIN/rview $PRIORITY_LEVEL
+update-alternatives --install $BIN/view view $VIM_BIN/view $PRIORITY_LEVEL
+update-alternatives --install $BIN/xxd xxd $VIM_BIN/xxd $PRIORITY_LEVEL
