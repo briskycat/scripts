af0675ac96d1775bef38107b9ca30d3e11ae801d
Byron Gibson <fbg111@gmail.com>
2015-08-05 10:14:16 -0700

    updated nodejs scripts for 0.12.7

--- install/node.js/node-install.sh
+++ install/node.js/node-install.sh
@@ -1,20 +1,24 @@
-#!/usr/bin/env bash -
+#!/usr/bin/env bash
 
-# install node.js to the system user via ubuntu update-alternatives,
-# rather than as current user via .profile.  system user in this case = /usr/bin
-# tested on Ubuntu 12.04 only
+# install node.js to the system user via ubuntu update-alternatives, rather than as current user via .profile.
+# System user in this case = /usr/bin tested on Ubuntu 12.04 only
 
 BIN="/usr/bin"
 MAN="/usr/share/man"
-NODE="/opt/nodejs/0.10.26"
+NODE="/opt/nodejs/0.12.7"
 NODE_BIN="$NODE/bin"
 NODE_MAN="$NODE/share/man"
+NPM="$NODE/lib/node_modules/npm"
+NPM_BIN="$NODE_BIN"
+NPM_MAN="$NPM/man"
 PRIORITY_LEVEL=1200
 
 update-alternatives --install $BIN/node nodejs $NODE_BIN/node $PRIORITY_LEVEL \
  --slave $BIN/npm npm $NODE_BIN/npm \
- --slave $BIN/nperf nperf $NODE_BIN/nperf \
- --slave $BIN/idonethis idonethis $NODE_BIN/idonethis \
- --slave $BIN/keybase keybase $NODE_BIN/keybase \
  --slave $BIN/keybase-installer keybase-installer $NODE_BIN/keybase-installer \
- --slave $MAN/man10 man.nodejs $NODE_MAN/man1
+ --slave $BIN/keybase keybase $NODE_BIN/keybase \
+ --slave $MAN/man11 nodejs.man $NODE_MAN/man1 \
+ --slave $MAN/man12 npm.man1 $NPM_MAN/man1 \
+ --slave $MAN/man13 npm.man3 $NPM_MAN/man3 \
+ --slave $MAN/man14 npm.man5 $NPM_MAN/man5 \
+ --slave $MAN/man15 npm.man7 $NPM_MAN/man7
